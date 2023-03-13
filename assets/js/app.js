0d88080e9b96ba3c867136cb3cb9621e73d68812
katrine-mo-jensen <katrine.mo.jensen@gmail.com>
2023-03-13 13:37:52 +0100

    nte

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,6 @@
 let myFadeImages = document.getElementsByClassName("fadeImages");
+let foldOutTrigger = document.getElementById("foldout-trigger");
+let foldOutText = document.getElementById("foldout-text");
 
 window.addEventListener("load", () => {
   console.log("loaded");
@@ -8,8 +10,7 @@ window.addEventListener("load", () => {
   for (const myElement of myFadeImages) {
     //console.log(myElement);
     myElement.classList.toggle("showImage");
-let foldOutTrigger = document.getElementById("foldout-trigger");
-let foldOutText = document.getElementById("foldout-text");
+
 
 foldOutTrigger.addEventListener("click", () => {
   foldOutText.classList.toggle("show-foldout");
