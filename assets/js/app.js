bbf9a2664627361b51005c6425d84fdf8a5e1d69
jermely <62332835+jermely@users.noreply.github.com>
2023-03-13 17:37:59 +0100

    accordion styled and containers added

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,9 +17,15 @@ let foldOutText = document.getElementById("foldout-text");
 foldOutTrigger.addEventListener("click", () => {
   foldOutText.classList.toggle("show-foldout");
 
-  if (foldOutTrigger.innerHTML == '<i class="fa-solid fa-chevron-down"></i>') {
+  if (foldOutText.classList.contains("show-foldout")) {
     foldOutTrigger.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
   } else {
     foldOutTrigger.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
   }
+
+  /* if (foldOutTrigger.innerHTML == '<i class="fa-solid fa-chevron-down"></i>') {
+    foldOutTrigger.innerHTML = '<i class="fa-solid fa-chevron-up"></i>';
+  } else {
+    foldOutTrigger.innerHTML = '<i class="fa-solid fa-chevron-down"></i>';
+  } */
 });
